e7fda40af6e52ceb269d2011423dd1d5a8ad177f
amiralii <mr.roshanae@gmail.com>
2019-02-16 01:44:38 +0330

    fixed

--- publish/main.go
+++ publish/main.go
@@ -5,13 +5,16 @@ import (
 	pb "github.com/amiraliio/publishen/publish/model/publish"
 	"github.com/micro/go-log"
 	"github.com/micro/go-micro"
+	"time"
 )
 
 func main() {
 
 	srv := micro.NewService(
 		micro.Name("publishen_publish_service"),
-		micro.Version("1.0"), 
+		micro.Version("1.0.0"),
+		micro.RegisterTTL(time.Second*30),
+		micro.RegisterInterval(time.Second*15),
 	)
 
 	srv.Init()
